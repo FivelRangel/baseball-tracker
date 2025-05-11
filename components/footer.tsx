23f4d35a5f50c3dffd5be21b9885068e322d4e7b
FivelRangel <154839566+FivelRangel@users.noreply.github.com>
2025-05-11 00:09:29 -0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="mt-8 text-center text-gray-500 text-sm pb-4 flex flex-col items-center">
       <div className="flex items-center justify-center mb-2">
-        <Baseball className="h-4 w-4 text-baseball-red mr-1" />
+        <span className="text-baseball-red text-xl mr-1">🥎</span>
         <span className="font-bebas">STRIKES & BALLS</span>
       </div>
       <div>Tu pizarra digital</div>
